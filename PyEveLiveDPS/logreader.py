f9db554b7c767ec9af328ff018dad8ac51d8da5d
Aeternitas26 <82459247+Aeternitas26@users.noreply.github.com>
2026-01-05 23:00:21 +0100

    Update logreader.py
    Mining update Nov. 2025

--- PyEveLiveDPS/logreader.py
+++ PyEveLiveDPS/logreader.py
@@ -52,7 +52,7 @@ _logLanguageRegex = {
         'capTransferedIn': "\(combat\) <.*?><b>([0-9]+).*> remote capacitor transmitted by <",
         'capNeutralizedIn': "\(combat\) <.*?ffe57f7f><b>([0-9]+).*> energy neutralized <",
         'nosTaken': "\(combat\) <.*?><b>\-([0-9]+).*> energy drained to <",
-        'mined': "\(mining\) .*? <.*?><.*?>([0-9]+).*> units of <.*?><.*?>(.+?)<"
+        'mined': "\(mining\) .*? <.*?><.*?>([0-9]+).*> units of <.*?><.*?>(.+?)"
     },
     'russian': {
         'character': "(?<=Слушатель: ).*",
@@ -548,3 +548,4 @@ def ProcessCharacterLine(characterLine):
         if character:
             return character.group(0), language
     raise BadLogException("not character log")
+
